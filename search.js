b6290a040a919cc9e48d9d458858b024ff969a34
Phalyaj5119 <123476852+Phalyaj5119@users.noreply.github.com>
2024-02-12 12:42:41 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,11 @@
+document.getElementById("searchButton").addEventListener("click", function() {
+    var searchInputValue = document.getElementById("searchInput").value;
+    // Here you can perform any action with the search input value,
+    // such as displaying search results or redirecting the user to a search results page
+    console.log("Search term:", searchInputValue);
+});document.getElementById("searchButton").addEventListener("click", function() {
+            var searchInputValue = document.getElementById("searchInput").value;
+            // Here you can perform any action with the search input value,
+            // such as displaying search results or redirecting the user to a search results page
+            console.log("Search term:", searchInputValue);
+        });
